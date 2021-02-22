ca96499b0ca1e1ab050d59b37eb2a13ec4e7e428
Kozionov <e-kozionov@mail.ru>
2021-02-22 17:02:33 +1200

    Initial Commit

---
+++ Lesson_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_01
+//
+//  Created by Evgeniy Kozionov on 22.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
